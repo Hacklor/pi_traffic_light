25b9e7391ce1d4742795991fb2d97e2a5f537093
Nicole Bekkers <hackybekkers@hacklor.com>
2013-09-14 20:54:41 +0000

    Unending animation of a traffic light

--- light.rb
+++ light.rb
@@ -1,18 +1,51 @@
 require 'pi_piper'
 
-puts "Initializing lights"
-red = PiPiper::Pin.new(:pin => 17, :direction => :out)
-yellow = PiPiper::Pin.new(:pin => 27, :direction => :out)
-green = PiPiper::Pin.new(:pin => 22, :direction => :out)
+class TrafficLight
 
-puts "Lights on"
-red.on
-yellow.on
-green.on
+  def initialize
+    @red = PiPiper::Pin.new(:pin => 17, :direction => :out)
+    @yellow = PiPiper::Pin.new(:pin => 27, :direction => :out)
+    @green = PiPiper::Pin.new(:pin => 22, :direction => :out)
+  end
 
-sleep 5
+  def green
+    @red.off
+    @yellow.off
+    @green.on
+  end
 
-puts "lights off"
-red.off
-yellow.off
-green.off
+  def yellow
+    @red.off
+    @yellow.on
+    @green.off
+  end
+
+  def red
+    @red.on
+    @yellow.off
+    @green.off
+  end
+
+  def off
+    @red.off
+    @yellow.off
+    @green.off
+  end
+end
+
+
+begin
+  puts "Initializing traffic light"
+  traffic_light = TrafficLight.new
+
+  while true do
+    traffic_light.red
+    sleep 3
+    traffic_light.green
+    sleep 3
+    traffic_light.yellow
+    sleep 3
+  end
+ensure
+  traffic_light.off
+end
